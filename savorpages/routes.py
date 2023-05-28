36c13274142f08b52b380796172135b28764d69d
monimaj89 <mn.majchrowicz@gmail.com>
2023-05-28 20:58:56 +0000

    Add register template

--- savorpages/routes.py
+++ savorpages/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from werkzeug.security import generate_password_hash, check_password_hash
 from savorpages import app, db
 from savorpages.models import Category, Users
 
@@ -11,3 +12,8 @@ def home():
 @app.route("/recipes")
 def recipes():
     return render_template("recipes.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
