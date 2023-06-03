c3ce7da6b38e8122570b080964a9345339ad05ff
monimaj89 <mn.majchrowicz@gmail.com>
2023-06-03 15:26:05 +0000

    Add a schema for a recipe model

--- savorpages/models.py
+++ savorpages/models.py
@@ -20,3 +20,21 @@ class Users(db.Model):
     def __rep__(self):
         # __repr__ to represent itself in the form of a string
         return self.user_name
+
+
+class Recipe(db.Model):
+    # schema for the Recipe model
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_name = db.Column(db.String(50), unique=True, nullable=False)
+    recipe_description = db.Column(db.Text, nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    preparation = db.Column(db.Text, nullable=False)
+    cook_time = db.Column(db.Integer, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id",
+                            ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return "#{0} - Recipe: {1}".format(
+            self.id, self.recipe_name
+        )
